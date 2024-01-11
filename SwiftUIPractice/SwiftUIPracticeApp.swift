647a8bcf10024294d0319bd63dd174733e97e406
Harish Garg <harishgarg259@gmail.com>
2024-01-11 09:34:57 -0800

    Initial commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Harish Garg on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
